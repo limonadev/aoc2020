94fb81b8d9a3861800c1917bbd0f0496eab5fc57
limonadev <limonadev@whiteleaf.dev>
2020-12-04 21:22:40 -0500

    Added day 3 to bin

--- bin/day_selector.dart
+++ bin/day_selector.dart
@@ -4,6 +4,7 @@ import 'package:aoc2020/days/all.dart';
 enum DayOption {
   Day01,
   Day02,
+  Day03,
 }
 
 class DaySelector {
@@ -15,6 +16,9 @@ class DaySelector {
         break;
       case DayOption.Day02:
         result = Day2(inputPath: 'assets/day_2.txt');
+        break;
+      case DayOption.Day03:
+        result = Day3(inputPath: 'assets/day_3.txt');
         break;
       default:
         throw ArgumentError('$selection is not part of the options');
--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,7 @@
 import 'day_selector.dart';
 
 void main() async {
-  final selected = DaySelector.choose(selection: DayOption.Day02);
+  final selected = DaySelector.choose(selection: DayOption.Day03);
   final result = await selected.solve();
 
   print(result);
