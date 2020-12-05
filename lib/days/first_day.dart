fa3ee47ac470e6c4437ff760dfc781fd0898294f
limonadev <limonadev@whiteleaf.dev>
2020-12-04 20:21:51 -0500

    Added second part of first exercise

--- lib/days/first_day.dart
+++ lib/days/first_day.dart
@@ -3,18 +3,27 @@ import 'dart:io';
 import 'package:aoc2020/days/day.dart';
 import 'package:meta/meta.dart';
 
-class FirstDay extends Day<int> {
+class FirstDay extends Day<Map<String, int>> {
   FirstDay({@required this.inputPath});
 
   final String inputPath;
 
   @override
-  Future<int> solve() async {
-    final needed = <int>{};
-    int result;
+  Future<Map<String, int>> solve() async {
+    final result = <String, int>{};
 
     final file = File(inputPath);
     final lines = file.readAsLinesSync();
+
+    result['first'] = _firstPart(lines);
+    result['second'] = _secondPart(lines);
+
+    return result;
+  }
+
+  int _firstPart(List<String> lines) {
+    int result;
+    final needed = <int>{};
 
     for (final line in lines) {
       final number = int.parse(line);
@@ -26,6 +35,32 @@ class FirstDay extends Day<int> {
       }
 
       needed.add(number);
+    }
+
+    return result;
+  }
+
+  int _secondPart(List<String> lines) {
+    int result;
+
+    final numbers = lines.map((l) => int.parse(l)).toList();
+    final rest = Set.from(numbers);
+
+    for (var i = 0; i < numbers.length; i++) {
+      for (var j = i + 1; j < numbers.length; j++) {
+        final currentSum = numbers[i] + numbers[j];
+
+        if (currentSum > 2020) {
+          continue;
+        }
+
+        final need = 2020 - currentSum;
+
+        if (rest.contains(need)) {
+          result = numbers[i] * numbers[j] * need;
+          break;
+        }
+      }
     }
 
     return result;
