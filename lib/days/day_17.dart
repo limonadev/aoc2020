99590b05efa861772e65403a9fda800aa44b6a26
limonadev <limonadev@whiteleaf.dev>
2020-12-17 10:06:12 -0500

    Fixed first solution to use the generalized version

--- lib/days/day_17.dart
+++ lib/days/day_17.dart
@@ -65,39 +65,43 @@ class Day17 extends Day<Map<String, int>> {
   }
 
   int _firstPart(List<String> lines) {
-    var grid = <List<List<int>>>[];
+    var activePoints = <NPoint>{};
 
-    for (final line in lines) {
-      final row = <List<int>>[];
-
-      for (var i = 0; i < line.length; i++) {
-        row.add([line[i] == '#' ? 1 : 0]);
+    for (var i = 0; i < lines.length; i++) {
+      final line = lines[i];
+      for (var j = 0; j < line.length; j++) {
+        if (line[j] == '#') {
+          activePoints.add(NPoint(coords: [i, j, 0]));
+        }
       }
-
-      grid.add(row);
     }
 
-    for (var counter = 0; counter < 6; counter++) {
-      _addLayer(grid);
-      var copy = _copyGrid(grid);
+    for (var i = 0; i < 6; i++) {
+      final candidates = <NPoint, int>{};
+      final nextActivePoints = <NPoint>{};
 
-      for (var i = 0; i < grid.length; i++) {
-        for (var j = 0; j < grid[i].length; j++) {
-          for (var k = 0; k < grid[i][j].length; k++) {
-            final active = _countActiveNeighbor(grid, i, j, k);
-            if (grid[i][j][k] == 0 && active == 3) {
-              copy[i][j][k] = 1;
-            } else if (grid[i][j][k] == 1 && active != 2 && active != 3) {
-              copy[i][j][k] = 0;
-            }
-          }
+      for (final point in activePoints) {
+        final activeNeighbors = _countActive3DNeighbor(
+          point,
+          activePoints,
+          candidates,
+        );
+
+        if (activeNeighbors == 2 || activeNeighbors == 3) {
+          nextActivePoints.add(point);
         }
       }
 
-      grid = copy;
+      for (final entry in candidates.entries) {
+        if (entry.value == 3) {
+          nextActivePoints.add(entry.key);
+        }
+      }
+
+      activePoints = nextActivePoints;
     }
 
-    return _totalActive(grid);
+    return activePoints.length;
   }
 
   int _secondPart(List<String> lines) {
@@ -140,76 +144,6 @@ class Day17 extends Day<Map<String, int>> {
     return activePoints.length;
   }
 
-  void _addLayer(List<List<List<int>>> grid) {
-    for (var i = 0; i < grid.length; i++) {
-      for (var j = 0; j < grid[i].length; j++) {
-        grid[i][j].insert(0, 0);
-        grid[i][j].add(0);
-      }
-    }
-
-    final elemToAdd = List.filled(grid[0][0].length, 0, growable: true);
-    for (var i = 0; i < grid.length; i++) {
-      grid[i].insert(0, List.from(elemToAdd));
-      grid[i].add(List.from(elemToAdd));
-    }
-
-    final rowToAdd = <List<int>>[];
-    for (var i = 0; i < grid[0].length; i++) {
-      rowToAdd.add(List.from(elemToAdd));
-    }
-
-    grid.insert(0, List.from(rowToAdd));
-    grid.add(List.from(rowToAdd));
-  }
-
-  int _countActiveNeighbor(List<List<List<int>>> grid, int i, int j, int k) {
-    var count = 0;
-
-    for (var mi = i - 1; mi <= i + 1; mi++) {
-      if (mi < 0 || mi >= grid.length) continue;
-      for (var mj = j - 1; mj <= j + 1; mj++) {
-        if (mj < 0 || mj >= grid[mi].length) continue;
-        for (var mk = k - 1; mk <= k + 1; mk++) {
-          if (mk < 0 || mk >= grid[mi][mj].length) continue;
-          if (i == mi && j == mj && k == mk) continue;
-
-          count += grid[mi][mj][mk];
-        }
-      }
-    }
-
-    return count;
-  }
-
-  List<List<List<int>>> _copyGrid(List<List<List<int>>> grid) {
-    final result = <List<List<int>>>[];
-
-    for (final row in grid) {
-      final r = <List<int>>[];
-      for (final col in row) {
-        r.add(List.from(col));
-      }
-      result.add(r);
-    }
-
-    return result;
-  }
-
-  int _totalActive(List<List<List<int>>> grid) {
-    var total = 0;
-
-    for (var i = 0; i < grid.length; i++) {
-      for (var j = 0; j < grid[i].length; j++) {
-        for (var k = 0; k < grid[i][j].length; k++) {
-          total += grid[i][j][k];
-        }
-      }
-    }
-
-    return total;
-  }
-
   int _countActive4DNeighbor(
     NPoint p,
     Set<NPoint> activePoints,
@@ -230,6 +164,32 @@ class Day17 extends Day<Map<String, int>> {
               candidates.putIfAbsent(neighbor, () => 0);
               candidates[neighbor]++;
             }
+          }
+        }
+      }
+    }
+
+    return total;
+  }
+
+  int _countActive3DNeighbor(
+    NPoint p,
+    Set<NPoint> activePoints,
+    Map<NPoint, int> candidates,
+  ) {
+    var total = 0;
+
+    for (var i = p.coords[0] - 1; i <= p.coords[0] + 1; i++) {
+      for (var j = p.coords[1] - 1; j <= p.coords[1] + 1; j++) {
+        for (var k = p.coords[2] - 1; k <= p.coords[2] + 1; k++) {
+          final neighbor = NPoint(coords: [i, j, k]);
+          if (neighbor == p) continue;
+
+          if (activePoints.contains(neighbor)) {
+            total++;
+          } else {
+            candidates.putIfAbsent(neighbor, () => 0);
+            candidates[neighbor]++;
           }
         }
       }
