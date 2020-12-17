178613d423ba3884494222a2e9125154afc9a63c
limonadev <limonadev@whiteleaf.dev>
2020-12-17 10:02:00 -0500

    Added day 17 second part solution

--- lib/days/day_17.dart
+++ lib/days/day_17.dart
@@ -3,6 +3,45 @@ import 'dart:io';
 import 'package:aoc2020/days/day.dart';
 import 'package:meta/meta.dart';
 
+class NPoint {
+  NPoint({@required List<int> coords}) : coords = List.from(coords);
+
+  final List<int> coords;
+
+  @override
+  int get hashCode {
+    var result = 17;
+
+    coords.forEach((coord) {
+      var objectHash = coord;
+      result = 31 * result + objectHash;
+    });
+
+    return result;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    var result = true;
+    if (other is NPoint && coords.length == other.coords.length) {
+      for (var i = 0; i < coords.length; i++) {
+        if (coords[i] != other.coords[i]) {
+          result = false;
+          break;
+        }
+      }
+    } else {
+      result = false;
+    }
+    return result;
+  }
+
+  @override
+  String toString() {
+    return '$coords';
+  }
+}
+
 class Day17 extends Day<Map<String, int>> {
   Day17({@required this.inputPath});
 
@@ -61,7 +100,45 @@ class Day17 extends Day<Map<String, int>> {
     return _totalActive(grid);
   }
 
-  int _secondPart(List<String> lines) {}
+  int _secondPart(List<String> lines) {
+    var activePoints = <NPoint>{};
+
+    for (var i = 0; i < lines.length; i++) {
+      final line = lines[i];
+      for (var j = 0; j < line.length; j++) {
+        if (line[j] == '#') {
+          activePoints.add(NPoint(coords: [i, j, 0, 0]));
+        }
+      }
+    }
+
+    for (var i = 0; i < 6; i++) {
+      final candidates = <NPoint, int>{};
+      final nextActivePoints = <NPoint>{};
+
+      for (final point in activePoints) {
+        final activeNeighbors = _countActive4DNeighbor(
+          point,
+          activePoints,
+          candidates,
+        );
+
+        if (activeNeighbors == 2 || activeNeighbors == 3) {
+          nextActivePoints.add(point);
+        }
+      }
+
+      for (final entry in candidates.entries) {
+        if (entry.value == 3) {
+          nextActivePoints.add(entry.key);
+        }
+      }
+
+      activePoints = nextActivePoints;
+    }
+
+    return activePoints.length;
+  }
 
   void _addLayer(List<List<List<int>>> grid) {
     for (var i = 0; i < grid.length; i++) {
@@ -126,6 +203,34 @@ class Day17 extends Day<Map<String, int>> {
       for (var j = 0; j < grid[i].length; j++) {
         for (var k = 0; k < grid[i][j].length; k++) {
           total += grid[i][j][k];
+        }
+      }
+    }
+
+    return total;
+  }
+
+  int _countActive4DNeighbor(
+    NPoint p,
+    Set<NPoint> activePoints,
+    Map<NPoint, int> candidates,
+  ) {
+    var total = 0;
+
+    for (var i = p.coords[0] - 1; i <= p.coords[0] + 1; i++) {
+      for (var j = p.coords[1] - 1; j <= p.coords[1] + 1; j++) {
+        for (var k = p.coords[2] - 1; k <= p.coords[2] + 1; k++) {
+          for (var l = p.coords[3] - 1; l <= p.coords[3] + 1; l++) {
+            final neighbor = NPoint(coords: [i, j, k, l]);
+            if (neighbor == p) continue;
+
+            if (activePoints.contains(neighbor)) {
+              total++;
+            } else {
+              candidates.putIfAbsent(neighbor, () => 0);
+              candidates[neighbor]++;
+            }
+          }
         }
       }
     }
